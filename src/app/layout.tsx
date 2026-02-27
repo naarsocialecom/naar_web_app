c5b359e4aefb7f27108f97d11e84393f24039479
rithikr1 <rithikrajesh@grethena.com>
2026-02-27 15:38:58 +0530

    Meta events

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,24 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           href="https://api.fontshare.com/v2/css?f[]=lufga@400,500,600,700&display=swap"
           rel="stylesheet"
         />
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '608485085693451');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        {/* End Meta Pixel Code */}
       </head>
       <body>
         <nav className="bg-[rgb(63,240,255)] py-1 px-6 text-xs font-medium text-black overflow-hidden whitespace-nowrap">
